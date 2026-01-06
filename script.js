dbc7874e0549423438e4c1b56a9093d6582f7163
Jamal Nadeem <jamalnadeem2004@gmail.com>
2026-01-07 01:14:14 +0500

    feat: Chapter 9-11

--- script.js
+++ script.js
@@ -326,20 +326,183 @@
 // document.writeln("Marks Obtained: " + marksObtained + "<br>");
 // document.writeln("Percentage: " + percentage.toFixed(2) + "%<br>");
 
-var subejct1 = prompt("Enter name of Subject 1:");
-var subejct2 = prompt("Enter name of Subject 2:");
-var subejct3 = prompt("Enter name of Subject 3:");
-var totalMarksPerSubject = 100;
-var totalMarks = totalMarksPerSubject * 3;
-var marks1 = parseFloat(prompt("Enter marks obtained in " + subejct1 + ":"));
-var marks2 = parseFloat(prompt("Enter marks obtained in " + subejct2 + ":"));
-var marks3 = parseFloat(prompt("Enter marks obtained in " + subejct3 + ":"));
+
+// // Class Task
+// var subejct1 = prompt("Enter name of Subject 1:");
+// var subejct2 = prompt("Enter name of Subject 2:");
+// var subejct3 = prompt("Enter name of Subject 3:");
+// var totalMarksPerSubject = 100;
+// var totalMarks = totalMarksPerSubject * 3;
+// var marks1 = parseFloat(prompt("Enter marks obtained in " + subejct1 + ":"));
+// var marks2 = parseFloat(prompt("Enter marks obtained in " + subejct2 + ":"));
+// var marks3 = parseFloat(prompt("Enter marks obtained in " + subejct3 + ":"));
+// var marksObtained = marks1 + marks2 + marks3;
+// var percentage = (marksObtained / totalMarks) * 100;
+// document.writeln("<h1>Marks Sheet</h1>");
+// document.writeln("<table border='1'><tr><th>Subject</th><th>Total Marks</th><th>Marks Obtained</th><th>Percentage</th></tr>");
+// document.writeln("<tr><td>" + subejct1 + "</td><td>" + totalMarksPerSubject + "</td><td>" + marks1 + "</td><td>" + ((marks1 / totalMarksPerSubject) * 100).toFixed(2) + "%</td></tr>");
+// document.writeln("<tr><td>" + subejct2 + "</td><td>" + totalMarksPerSubject + "</td><td>" + marks2 + "</td><td>" + ((marks2 / totalMarksPerSubject) * 100).toFixed(2) + "%</td></tr>");
+// document.writeln("<tr><td>" + subejct3 + "</td><td>" + totalMarksPerSubject + "</td><td>" + marks3 + "</td><td>" + ((marks3 / totalMarksPerSubject) * 100).toFixed(2) + "%</td></tr>");
+// document.writeln("<tr><th>Total</th><th>" + totalMarks + "</th><th>" + marksObtained + "</th><th>" + percentage.toFixed(2) + "%</th></tr>");
+// document.writeln("</table>");
+
+// // // Chapter 9-11
+// // // 1. Write a program to take “city” name as input from user.If user enters “Karachi”, welcome the user like this:“Welcome to city of lights”
+var city = prompt("Enter city name:");
+if (city === "karachi") {
+    alert("Welcome to city of lights");
+}
+
+// // // 2. Write a program to take "gender" as input from user. If the user is male, show "Good Morning Sir" and if the user is female, show "Good Morning Ma'am".
+var gender = prompt("Enter your gender:");
+if (gender === "male") {
+    alert("Good Morning Sir");
+} else if (gender === "female") {
+    alert("Good Morning Ma'am");
+}
+
+// // // 3. Write a program to take input color of road traffic signal from the user & show the message according to this table:
+var signalColor = prompt("Enter traffic signal color (red, yellow, green):");
+if (signalColor === "red") {
+    alert("Must Stop");
+} else if (signalColor === "yellow") {
+    alert("Ready to move");
+} else if (signalColor === "green") {
+    alert("Move now");
+}
+
+// // // 4. Write a program to take input remaining fuel in car (in litres) from user.If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car”.
+var fuel = parseFloat(prompt("Enter remaining fuel in car (in litres):"));
+if (fuel < 0.25) {
+    alert("Please refill the fuel in your car");
+}
+
+// // // 5. Run this script, & check whether alert message would be displayed or not. Record the outputs.
+// // // a. 
+var a = 4;
+if (++a === 5) {
+    alert("given condition for variable a is true");
+}
+// // // b.
+var b = 82;
+if (b++ === 83) {
+    alert("given condition for variable b is true");
+}
+// // // c.
+var c = 12;
+if (c++ === 13) {
+    alert("condition 1 is true");
+}
+if (c === 13) {
+    alert("condition 2 is true");
+}
+if (++c < 14) {
+    alert("condition 3 is true");
+}
+if (c === 14) {
+    alert("condition 4 is true");
+}
+// // // d.
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost) {
+    alert("The cost equals");
+}
+// // // e.
+if (true) {
+    alert("True");
+}
+if (false) {
+    alert("False");
+}
+// // // f.
+if ("car" < "cat") {
+    alert("car is smaller than cat");
+}
+
+// // // 6. Write a program to take input the marks obtained in three subjects & total marks. Compute & show the resulting percentage on your page. Take percentage & compute grade as per following table:
+var marks1 = parseFloat(prompt("Enter marks obtained in Subject 1:"));
+var marks2 = parseFloat(prompt("Enter marks obtained in Subject 2:"));
+var marks3 = parseFloat(prompt("Enter marks obtained in Subject 3:"));
+var totalMarks = parseFloat(prompt("Enter total marks:"));
 var marksObtained = marks1 + marks2 + marks3;
 var percentage = (marksObtained / totalMarks) * 100;
+var grade, remarks;
+if (percentage >= 80) {
+    grade = "A-one";
+    remarks = "Excellent";
+} else if (percentage >= 70) {
+    grade = "A";
+    remarks = "Good";
+} else if (percentage >= 60) {
+    grade = "B";
+    remarks = "You need to improve";
+} else {
+    grade = "Fail";
+    remarks = "Sorry";
+}
 document.writeln("<h1>Marks Sheet</h1>");
-document.writeln("<table border='1'><tr><th>Subject</th><th>Total Marks</th><th>Marks Obtained</th><th>Percentage</th></tr>");
-document.writeln("<tr><td>" + subejct1 + "</td><td>" + totalMarksPerSubject + "</td><td>" + marks1 + "</td><td>" + ((marks1 / totalMarksPerSubject) * 100).toFixed(2) + "%</td></tr>");
-document.writeln("<tr><td>" + subejct2 + "</td><td>" + totalMarksPerSubject + "</td><td>" + marks2 + "</td><td>" + ((marks2 / totalMarksPerSubject) * 100).toFixed(2) + "%</td></tr>");
-document.writeln("<tr><td>" + subejct3 + "</td><td>" + totalMarksPerSubject + "</td><td>" + marks3 + "</td><td>" + ((marks3 / totalMarksPerSubject) * 100).toFixed(2) + "%</td></tr>");
-document.writeln("<tr><th>Total</th><th>" + totalMarks + "</th><th>" + marksObtained + "</th><th>" + percentage.toFixed(2) + "%</th></tr>");
-document.writeln("</table>");
+document.writeln("Total Marks: " + totalMarks + "<br>");
+document.writeln("Marks Obtained: " + marksObtained + "<br>");
+document.writeln("Percentage: " + percentage + "%<br>");
+document.writeln("Grade: " + grade + "<br>");
+document.writeln("Remarks: " + remarks + "<br>");
+
+// // // 7. Guess game: Store a secret number (ranging from 1 to 10) in a variable.Then ask user to guess the secret number.If user guesses the same number, show “Bingo! Correct answer”.If the guessed number +1 is the secret number, show “Close enough to the correct answer”.
+var secretNumber = 7;
+var userGuess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+if (userGuess === secretNumber) {
+    alert("Bingo! Correct answer");
+} else if (userGuess === secretNumber + 1) {
+    alert("Close enough to the correct answer");
+}
+
+// // // 8. Write a program to check whether the given number is divisible by 3.If it is, show the message to the user “The number is divisible by 3”.Otherwise show “The number is not divisible by 3”.
+var number = parseInt(prompt("Enter a number to check divisibility by 3:"));
+if (number % 3 === 0) {
+    alert("The number is divisible by 3");
+} else {
+    alert("The number is not divisible by 3");
+}
+
+// // // 9. Write a program that checks whether the given input is an even number or an odd number.
+var num = parseInt(prompt("Enter a number to check if it is even or odd:"));
+if (num % 2 === 0) {
+    alert("The number is even");
+} else {
+    alert("The number is odd");
+}
+
+// // // 10. Write a program that takes temperature as input and shows a message based on following criteria:
+// // // a. T > 40 then “It is too hot outside.”
+// // // b. T > 30 then “The Weather today is Normal.”
+var temperature = parseFloat(prompt("Enter the temperature:"));
+if (temperature > 40) {
+    alert("It is too hot outside.");
+} else if (temperature > 30) {
+    alert("The Weather today is Normal.");
+} else if (temperature > 20) {
+    alert("Today’s Weather is cool.");
+} else if (temperature > 10) {
+    alert("OMG! Today’s weather is so Cool.");
+}
+
+// // // 11. Write a program to create a calculator for +, -, *, / & % using if statements.Take the following input:
+var num1 = parseFloat(prompt("Enter first number:"));
+var num2 = parseFloat(prompt("Enter second number:"));
+var operator = prompt("Enter an operator (+, -, *, /, %):");
+var result;
+if (operator === "+") {
+    result = num1 + num2;
+} else if (operator === "-") {
+    result = num1 - num2;
+} else if (operator === "*") {
+    result = num1 * num2;
+} else if (operator === "/") {
+    result = num1 / num2;
+} else if (operator === "%") {
+    result = num1 % num2;
+}
+alert("The result of " + num1 + " " + operator + " " + num2 + " is: " + result);
+
