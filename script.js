58541b41b190795cf343916a8de2704ea3fd6efc
Jamal Nadeem <jamalnadeem2004@gmail.com>
2026-02-17 08:01:18 +0500

    feat: chapter 43-48

--- script.js
+++ script.js
@@ -1364,3 +1364,34 @@ function calculateCurrencyNotes(amount) {
         notes10: notes10
     };
 }
+// // // // // // // Chapter 43-48
+// // // // // // // 1. Show an alert box on click on a link.
+function showAlert() {
+    alert("You clicked the link!");
+}
+// // // // // // // 2. Display some Mobile images in browser. On click on an image Show the message in alert to user.
+function showImageAlert() {
+    alert("You clicked the image!");
+}
+// // // // // // // 3. Display 10 student records in table and each row should contain a delete button. If you click on a button to delete a record, entire row should be deleted.
+function deleteRow(button) {
+    var row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+// // // // // // // 4. Display an image in browser. Change the picture on mouseover and set the first picture on mouseout.
+function changeImageOnMouseOver(image) {
+    image.src = "https://img.freepik.com/free-photo/lavender-field-sunset-near-valensole_268835-3910.jpg?semt=ais_user_personalization&w=740&q=80";
+}
+function resetImageOnMouseOut(image) {
+    image.src = "https://www.shutterstock.com/image-photo/sun-sets-behind-mountain-ranges-600nw-2479236003.jpg";
+}
+// // // // // // // 5. Show a counter in browser. Counter should increase on click on increase button and decrease on click on decrease button. And show updated counter value in browser.
+var counterValue = 0;
+function increaseCounter() {
+    counterValue++;
+    document.getElementById("counter").innerText = counterValue;
+}
+function decreaseCounter() {
+    counterValue--;
+    document.getElementById("counter").innerText = counterValue;
+}
