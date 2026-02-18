66f89aacf7c488e57ba42abc7f9701386a4fba63
Jamal Nadeem <jamalnadeem2004@gmail.com>
2026-02-18 22:27:24 +0500

    feat: test commit

--- script.js
+++ script.js
@@ -1437,3 +1437,9 @@ function addStudent() {
 }
 
 // // // // // // // Chapter 53-58
+// // // // // // // 1. Show an image in full size on click on a thumbnail image.
+function showFullSizeImage(thumbnail) {
+    var fullSizeImage = document.getElementById("fullSizeImage");
+    fullSizeImage.src = thumbnail.src;
+    fullSizeImage.style.display = "block";
+}
